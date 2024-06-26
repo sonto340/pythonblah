c61ceb28afa2b1c7cabaed8e832b569f2825934f
sonto <sonto@gmail.com>
2024-06-25 19:56:37 -0500

    syntax

--- playground/playgrounds_lists.py
+++ playground/playgrounds_lists.py
@@ -1,3 +1,5 @@
-from playground import matrix 
-stats = [matrix[int(textnum1)][int(testnum2)]]
+from playground import genmatrix 
+from playground import testnum1
+from playground import testnum2
+stats = [genmatrix[int(testnum1)][int(testnum2)]]
 print(stats)
