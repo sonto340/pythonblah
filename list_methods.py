3a825979991e525bd4bec3287f43e2289d78c338
sonto <sonto@gmail.com>
2024-06-25 20:52:42 -0500

    brainm hurts

--- list_methods.py
+++ list_methods.py
@@ -1,2 +1,16 @@
+numbers = True
 numbers = [5, 2, 1, 7, 4]
-numbers.append()
+numbers2 = numbers.copy
+numbers.append(10)
+print(numbers)
+print(numbers.copy)
+
+
+check = 50 in numbers
+
+machine_on = check
+
+if machine_on:
+    print("The machine is on.")
+elif machine_on == False:
+    print("The machine is off.")
---
+++ numbers_dupe.py
@@ -0,0 +1,6 @@
+numbers = [1, 5, 7, 4, 3, 7, 9, 8, 7, 6, 5, 5, 4, 3]
+uniques = []
+for strings in numbers:
+    if strings not in uniques:
+        uniques.append(strings)
+print(uniques)
