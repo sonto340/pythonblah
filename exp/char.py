be4c3c14289ccf19be620d06196ed555e6189326
Sonto340 <99784283+sonto340@users.noreply.github.com>
2024-06-27 04:05:02 +0000

    switching to pc

--- exp/char.py
+++ exp/char.py
@@ -1,9 +1,23 @@
-# character stats are included here 
+# character stats are included here
 # just trying some stuff
 # stats
 from main import player_name
-name = player_name 
+name = player_name
 atk = 15
 udef = 10
 hp = 50
 acc = 75
+
+atk_matrix = 0
+
+if len(name) > 3:
+    m = 3
+    2m = 4
+    3m = 5
+  
+else:
+  atk_matrix == 7
+
+matrix[
+  []
+]
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 numbers = [4, 6, 7, 7, 7, 7, 5, 5, 4, 4, 4]
 removed = []
 for number in numbers:
-    if number is not removed:
+    if number not in removed:
         removed.append(number)
         numbers.remove(number)
 print(removed)
